0b2b47dacaf69996871d8fdf4b28979b785fac3d
ad3sh <adeshkolte@gmail.com>
2022-12-15 16:02:34 +0530

    addded

---
+++ build/precache-manifest.4504013c3afdd146579a5f55d5f96909.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d36a53f02ef5058a0349f2862da028a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39dd71c65442f65120da",
+    "url": "/static/css/main.2a12f11a.chunk.css"
+  },
+  {
+    "revision": "43de866c9dd22648d57b",
+    "url": "/static/js/2.e3632047.chunk.js"
+  },
+  {
+    "revision": "39dd71c65442f65120da",
+    "url": "/static/js/main.42c8a105.chunk.js"
+  },
+  {
+    "revision": "f67c199efb9f2c29f389",
+    "url": "/static/js/runtime-main.f294effb.js"
+  }
+]);
